410b4a516a3b3b168a974d5a195bc73a140b5730
Hector Bustillos <hector.bustillos@crowdint.com>
2013-07-18 22:14:35 -0500

    add new relic

--- config/initializers/new_relic.rb
+++ config/initializers/new_relic.rb
@@ -2,3 +2,7 @@
 # This is needed when using Unicorn and preload_app is not set to true.
 # See http://support.newrelic.com/kb/troubleshooting/unicorn-no-data
 #NewRelic::Agent.after_fork(:force_reconnect => true) if ENV['NEW_RELIC_APP_NAME']
+if defined? Unicorn
+  ::NewRelic::Agent.manual_start()
+  ::NewRelic::Agent.after_fork(:force_reconnect => true)
+end
