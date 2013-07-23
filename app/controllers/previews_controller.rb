801d269bc7edc93ec1170e20fcfbb00775fe9f58
Hector Bustillos <hector.bustillos@crowdint.com>
2013-07-22 22:30:27 -0500

    add namespace to show on previews controller:

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -1,6 +1,6 @@
 class PreviewsController < ApplicationController
   def show
-    @post = Post.includes(:author).find(params[:id])
+    @post = Crowdblog::Post.includes(:author).find(params[:id])
     @post.published_at = Date.today
     render 'crowdblog/posts/show'
   end
